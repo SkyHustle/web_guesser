4657934383a741270ceea6091441b603985c2d00
Dmitry Sky <dmitry.sky@icloud.com>
2015-05-13 14:11:08 -0600

    added cheat mode and resized h1 text

--- web_guesser.rb
+++ web_guesser.rb
@@ -10,9 +10,12 @@ get '/' do
 end
 
 def guess_checker(guess, random_number)
-  return "Guess a number from 0 to 50 Cheat: #{random_number}" if guess == nil
-  guess = guess.to_i
-    if    guess - random_number > 5
+  return "Guess a number from 0 to 50" if guess == nil
+  return "Cheater!! The winning number is #{random_number}" if guess.downcase == "cheat"
+  return "Input field takes a number up to 2-digits" if guess.match(/[a-zA-Z]/) != nil
+  
+  guess = guess.to_i  
+    if guess - random_number > 5
       "#{guess} is Way too high!"
     elsif random_number < guess
       "#{guess} is Too high"
